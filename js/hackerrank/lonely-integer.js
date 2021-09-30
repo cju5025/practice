6eb58da817452dfed1dbcda1db4bb6b3b2f0d116
Colter Ulrich <cju5025@gmail.com>
2021-09-29 22:11:24 -0600

    lonely-integer-new-approach

--- js/hackerrank/lonely-integer.js
+++ js/hackerrank/lonely-integer.js
@@ -1,17 +1,11 @@
 const input = [0,0,1,2,1]
 
-function lonelyinteger(a) {
-    let counter = 0
-    for (let i = 0; i < a.length; i++){
-        for (let j = i + 1; j < a.length; j++){
-            if (a[i] === a[j]){
-                counter += 1
-            }
-            console.log(counter)
-            if (counter === 0){
-                return a[i]
-            }
-            counter = 0
+const lonelyinteger = (a) => {
+    let inputString = a.toString()
+
+    for (let i = 0; i < inputString.length; i++){
+        if (inputString.replaceAll(inputString[i], "").length === inputString.length - 1){
+            return inputString[i]
         }
     }
 }
