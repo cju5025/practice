8e9f92e74ce0b49c734bd104ae949b1d726e94aa
Colter Ulrich <cju5025@gmail.com>
2021-10-16 17:14:46 -0600

    Solves Pangrams

--- js/hackerrank/panagrams.js
+++
@@ -1,6 +0,0 @@
-const panagram = (string) => {
-
-}
-
-console.log(panagram("We promptly judged antique ivory buckles for the next prize"))
-console.log(panagram("We promptly judged antique ivory buckles for the prize"))
