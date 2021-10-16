73f8cb89f0cf99223760147af3d9e0c2e8a0db23
Colter Ulrich <cju5025@gmail.com>
2021-10-16 16:49:10 -0600

    Starts Panagram

---
+++ js/hackerrank/panagrams.js
@@ -0,0 +1,6 @@
+const panagram = (string) => {
+
+}
+
+console.log(panagram("We promptly judged antique ivory buckles for the next prize"))
+console.log(panagram("We promptly judged antique ivory buckles for the prize"))
