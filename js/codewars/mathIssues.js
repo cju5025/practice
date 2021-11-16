d7373853caee315420386d1cc772d8a24aad9fb2
Colter Ulrich <cju5025@gmail.com>
2021-11-15 22:57:38 -0700

    Solves Math.floor

--- js/codewars/mathIssues.js
+++ js/codewars/mathIssues.js
@@ -11,27 +11,28 @@ Math.ceil = function(number) {
 };
   
 Math.floor = function(number) {
-    return 0; // TODO: fix this
+  let inputArray = Number.isInteger(number) ? wholeNumberToArray(number) : decimalToArray(number)
+  inputArray = parseStringArray(inputArray)
+  return inputArray[0]
 };
 
 const decimalToArray = (number) => {
   return number.toString().split(".")
-}
+};
 
 const wholeNumberToArray = (number) => {
   return number.toString().split("")
-}
+};
 
 const parseStringArray = (arrayOfStrings) => {
   return arrayOfStrings.map(e => {
     return parseInt(e)
   })
-}
-
-
-
+};
 
   console.log(Math.round(2.4))
   console.log(Math.round(2.8))
   console.log(Math.ceil(2.8))
   console.log(Math.ceil(2.8))
+  console.log(Math.floor(2.8))
+  console.log(Math.floor(2.8))
