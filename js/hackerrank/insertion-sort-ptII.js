812e34642ec109f51c9a6cac2debcd9752eab857
Colter Ulrich <cju5025@gmail.com>
2021-12-08 22:21:54 -0700

    Solves Insertion Sort Part II

--- js/hackerrank/insertion-sort-ptII.js
+++ js/hackerrank/insertion-sort-ptII.js
@@ -1,5 +1,18 @@
 function insertionSort(n, arr) {
-    console.log(arr.join(' '))
+    for (let i = 0; i < arr.length; i++){
+        splicer(arr)
+        console.log(arr.join(' '))
+        }
+    }
+
+function splicer(arr){
+    for (let i = 0; i < arr.length; i++){
+        if (arr[i] > arr[i+1]){
+            arr.splice(i+2, 0, arr[i])
+            arr.splice(i, 1)
+        }
+    }
+    return arr
 }
 
 console.log(insertionSort(7, [3,4,7,5,6,2,1]))
