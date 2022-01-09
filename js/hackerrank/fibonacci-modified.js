31b22fbc2db3bf079b18c000f285efa73d225ab8
Colter Ulrich <cju5025@gmail.com>
2022-01-08 19:06:05 -0700

    Solves Fibonacci Modified

--- js/hackerrank/fibonacci-modified.js
+++ js/hackerrank/fibonacci-modified.js
@@ -1,5 +1,14 @@
 function fibonacciModified(t1, t2, n) {
-    
+    let t3
+
+    for (let i = 0; i <= n - 3; i++){
+        t3 = BigInt(t1 + (t2 * t2))
+        t1 = BigInt(t2)
+        t2 = BigInt(t3)
+    }
+
+    return t3
+
 }
 
-console.log(fibonacciModified(0,1,6))
+console.log(fibonacciModified(0,1,10))
