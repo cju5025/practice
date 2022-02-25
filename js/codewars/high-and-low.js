cc216684c08bbf4637529975e33753b17b45edf6
Colter Ulrich <cju5025@gmail.com>
2022-02-25 09:37:07 -0700

    Solves highAndLow

--- js/codewars/high-and-low.js
+++ js/codewars/high-and-low.js
@@ -1,5 +1,22 @@
 const highAndLow = (string) => {
-    return string
+    const inputArray = string.split(" ")
+    const arrayOfIntegers = letterToInteger(inputArray)
+    let low = arrayOfIntegers[0]
+    let high = arrayOfIntegers[0]
+
+    for (let i = 0; i < arrayOfIntegers.length; i++){
+        if (arrayOfIntegers[i] < low){
+            low = arrayOfIntegers[i]
+        }
+        if (arrayOfIntegers[i] > high){
+            high = arrayOfIntegers[i]
+        }
+    }
+    return `${high} ${low}`
+}
+
+const letterToInteger = (array) => {
+    return array.map(e => parseInt(e))
 }
 
 console.log(highAndLow("1 2 3 4 5"))
